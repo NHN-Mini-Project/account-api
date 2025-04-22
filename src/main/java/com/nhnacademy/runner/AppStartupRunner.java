e436e4890dfcef9e53f0d434d4cda9905842b227
조수아 <sua6458@gmail.com>
2025-04-22 20:23:13 +0900

    test

--- src/main/java/com/nhnacademy/runner/AppStartupRunner.java
+++ src/main/java/com/nhnacademy/runner/AppStartupRunner.java
@@ -6,6 +6,6 @@ import org.springframework.boot.ApplicationRunner;
 public class AppStartupRunner implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
-
+        //
     }
 }
